7d7d099dfd99aba811ab224a147b3577224d7f6a
racso2609 <oscar.lugo@cub3.com>
2022-07-31 00:11:27 -0400

    first commit

---
+++ elpa/doom-modeline-now-playing-20210831.1442/doom-modeline-now-playing-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "doom-modeline-now-playing" "20210831.1442" "Segment for Doom Modeline to show playerctl information" '((emacs "24.4") (doom-modeline "3.0.0") (async "1.9.3")) :commit "ef9158dfdf32e8eb789b69e7394d0bddaa68f42c" :authors '(("Ellis Kenyő" . "me@elken.dev")) :maintainer '("Ellis Kenyő" . "me@elken.dev") :url "https://github.com/elken/doom-modeline-now-playing")
---
+++ elpa/eshell-syntax-highlighting-20210429.413/eshell-syntax-highlighting-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "eshell-syntax-highlighting" "20210429.413" "Highlight eshell commands" '((emacs "25.1")) :commit "8e3a685fc6d97af79e1046e5b24385786d8e92f6" :authors '(("Alex Kreisher" . "akreisher18@gmail.com")) :maintainer '("Alex Kreisher" . "akreisher18@gmail.com") :keywords '("convenience") :url "https://github.com/akreisher/eshell-syntax-highlighting")
